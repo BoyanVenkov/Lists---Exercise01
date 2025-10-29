90d3a18e809119a56f9ac03b9519c6e56f255d9b
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-29 07:17:51 +0200

    Added a New Project

---
+++ 02. Change List/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> changeList = Console.ReadLine().Split().Select(int.Parse).ToList();
+string[] input = Console.ReadLine().Split().ToArray();
+
+while (input[0] != "end")
+{
+    if (input[0] == "Delete")
+    {
+        changeList.RemoveAll(n => n == int.Parse(input[1]));
+    }
+    if (input[0] == "Insert")
+    {
+        changeList.Insert(int.Parse(input[2]), int.Parse(input[1]));
+    }
+    input = Console.ReadLine().Split().ToArray();
+}
+Console.WriteLine(string.Join(" ", changeList));
