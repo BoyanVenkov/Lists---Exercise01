5cee3140e873a7835cf3987b3331b7b9deef3179
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-29 06:56:11 +0200

    Add project files.

---
+++ 01. Train/Program.cs
@@ -0,0 +1,24 @@
+﻿List<int> train = Console.ReadLine().Split().Select(int.Parse).ToList();
+int passengers = int.Parse(Console.ReadLine());
+string[] input = Console.ReadLine().Split().ToArray();
+
+while (input[0] != "end")
+{
+    if (input[0] == "Add")
+    {
+        train.Add(int.Parse(input[1]));
+    }
+    else
+    {
+        for (int i = 0; i < train.Count; i++)
+        {
+            if (train[i] + int.Parse(input[0]) <= passengers)
+            {
+                train[i] += int.Parse(input[0]);
+                break;
+            }
+        }
+    }
+    input = Console.ReadLine().Split().ToArray();
+}
+Console.WriteLine(string.Join(" ", train));
