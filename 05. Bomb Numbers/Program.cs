c1405387f2b52eb6eff48a8849f2b988906829f2
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-31 19:22:56 +0200

    Added a New Project

---
+++ 05. Bomb Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToList();
+
+        int[] bombInfo = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        int bombNumber = bombInfo[0];
+        int power = bombInfo[1];
+
+        while (numbers.Contains(bombNumber))
+        {
+            int index = numbers.IndexOf(bombNumber);
+
+            int leftIndex = Math.Max(index - power, 0);
+            int rightIndex = Math.Min(index + power, numbers.Count - 1);
+            numbers.RemoveRange(leftIndex, rightIndex - leftIndex + 1);
+        }
+
+        Console.WriteLine(numbers.Sum());
+    }
+}
