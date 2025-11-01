abe25977eb563af198e294b74edadcfb037682ae
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-01 07:35:32 +0200

    Added a New Project

--- 06. Cards Game/Program.cs
+++ 06. Cards Game/Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+List<int> player1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> player2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+while (player1.Count != 0 && player2.Count != 0)
+{
+    if (player1[0] > player2[0])
+    {
+        player1.Add(player1[0]);
+        player1.Add(player2[0]);
+        player1.RemoveAt(0);
+        player2.RemoveAt(0);
+        continue;
+    }
+    if (player2[0] > player1[0])
+    {
+        player2.Add(player2[0]);
+        player2.Add(player1[0]);
+        player1.RemoveAt(0);
+        player2.RemoveAt(0);
+        continue;
+    }
+    if (player1[0] == player2[0])
+    {
+        player1.RemoveAt(0);
+        player2.RemoveAt(0);
+        continue;
+    }
+}
+if (player2.Count == 0)
+{
+    int sum = 0;
+    foreach (int n in player1)
+    {
+        sum += n;
+    }
+    Console.WriteLine($"First player wins! Sum: {sum}");
+    return;
+}
+if (player1.Count == 0)
+{
+    int sum = 0;
+    foreach (int n in player2)
+    {
+        sum += n;
+    }
+    Console.WriteLine($"Second player wins! Sum: {sum}");
+    return;
+}
