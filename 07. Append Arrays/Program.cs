b98031c9d13e812295c90c750ad26d7771b93175
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-01 08:16:11 +0200

    Added a New Project

---
+++ 07. Append Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        List<string> arrays = input.Split('|', StringSplitOptions.RemoveEmptyEntries)
+                                   .ToList();
+
+        List<int> result = new List<int>();
+
+        for (int i = arrays.Count - 1; i >= 0; i--)
+        {
+            int[] numbers = arrays[i]
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            result.AddRange(numbers);
+        }
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
